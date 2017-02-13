eaeccb74b3377597fe23ea59a362b82da245f1d6
fdopen <andreashauptmann@t-online.de>
2017-02-13 17:30:22 +0100

    rewrite patches
    be compatible with 'patch -p1 < ...'

--- packages/re2/re2.113.33.00+4.03/files/patch_gen_install.ml
+++ packages/re2/re2.113.33.00+4.03/files/patch_gen_install.ml
@@ -1,6 +1,6 @@
 --- ./js-utils/gen_install.ml
 +++ ./js-utils/gen_install.ml
-@@ -30,10 +30,29 @@
+@@ -30,11 +30,30 @@
    lines_of_file "setup.data"
    |> List.map (fun line -> Scanf.sscanf line "%[^=]=%S" (fun k v -> (k, v)))
  
@@ -12,7 +12,7 @@
 +    Bytes.set b i (if c = '\\' then '/' else c)
 +  done;
 +  if l > 1 && s.[1] = ':' && (s.[0] >= 'a' && s.[0] <= 'z') &&
-+     (l = 2 || Bytes.get b 1 = '/' ) then (
++     (l = 2 || Bytes.get b 2 = '/' ) then (
 +    Bytes.set b 1 (Char.uppercase s.[0])
 +  );
 +  Bytes.unsafe_to_string b
@@ -23,11 +23,12 @@
 +  | _ -> fun id -> id
 +
  let remove_cwd =
--  let prefix = Sys.getcwd () ^ "/" in
-+  let prefix = slashify (Sys.getcwd () ^ "/") in
+-  let prefix = Sys.getcwd () ^ Filename.dir_sep in
++  let prefix = slashify (Sys.getcwd () ^ Filename.dir_sep) in
    let len_prefix = String.length prefix in
    fun fn ->
-+    let fn = slashify fn in
      let len = String.length fn in
++    let fn = slashify fn in
      if len >= len_prefix && String.sub fn 0 len_prefix = prefix then
        String.sub fn len_prefix (len - len_prefix)
+     else
