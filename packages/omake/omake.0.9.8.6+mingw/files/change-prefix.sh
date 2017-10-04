5f2a3fb5c3bf452802e76dcfdb50028d8f5056b7
fdopen <andreashauptmann@t-online.de>
2017-10-04 12:46:34 +0200

    omake.0.9.8.6+mingw winfix

--- packages/omake/omake.0.9.8.6+mingw/files/change-prefix.sh
+++ packages/omake/omake.0.9.8.6+mingw/files/change-prefix.sh
@@ -4,3 +4,4 @@ set -eux
 
 prefix="$(ocamlc -config | tr -d '\015' | awk '/^ranlib/ {print $2}' | sed 's|ranlib||g')"
 sed -i "s|public.GCCTOOLPREFIX=i686-w64-mingw32-|public.GCCTOOLPREFIX=${prefix}|g" lib/build/C.om
+sed -i 's|AR := $(AR)|AR := '"${prefix}ar|g" src/Makefile
