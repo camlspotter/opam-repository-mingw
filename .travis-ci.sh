a1aaedd4e4411b92ebec247e8a1fad1b695cc647
fdopen <andreashauptmann@t-online.de>
2018-03-05 14:29:46 +0100

    Merge pull request #11488 from jpdeplaix/dont-test-deps
    [Travis script] Don't test dependencies

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -100,6 +100,9 @@ function build_one {
     depext=$(opam depext -ls $pkg --no-sources)
     opam depext $pkg
     echo
+    echo "====== Installing dependencies ======"
+    opam install --deps-only $pkg
+    echo
     echo "====== Installing package ======"
     opam install -t $pkg
     opam remove -a ${pkg%%.*}
